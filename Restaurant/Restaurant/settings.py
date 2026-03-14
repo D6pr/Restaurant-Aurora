8432209b0619c00441c5e0008453366dedaa78d3
D6 <konctantun68@gmail.com>
2026-03-14 12:17:57 +0200

    Add new styles for navigation buttons and update static files
    - Introduced new CSS styles for navigation buttons, including minimum width and font weight adjustments.
    - Added outline styles for success, primary, and secondary button variants.
    - Created new CSS files for styles and their gzipped versions.
    - Updated staticfiles.json to include new paths for the added styles.

--- Restaurant/Restaurant/settings.py
+++ Restaurant/Restaurant/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'menu/static')]
 STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
 
 # WhiteNoise configuration
--- Restaurant/menu/models.py
+++ Restaurant/menu/models.py
@@ -14,7 +14,7 @@ class Dish(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    image = models.ImageField(upload_to='dishes/')
+    image = models.CharField(max_length=200)
     popularity = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
